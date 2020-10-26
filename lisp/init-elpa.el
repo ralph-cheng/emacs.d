5bb396d719077c2aeebf28a642b2ccf3b557c28a
Cheng, Rong <ralph_cheng@hotmail.com>
2020-10-26 21:33:19 +0800

    setup package repository mirrors

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -15,10 +15,12 @@
 
 ;;; Standard package repositories
 
-(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+;;(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
 ;; Official MELPA Mirror, in case necessary.
 ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
-
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("org"   . "http://elpa.emacs-china.org/org/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 
 
 ;; Work-around for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
